82c0fe6c98dff0683471c04eec97bc475ded5407
Danish1103 <138514710+Danish1103@users.noreply.github.com>
2023-07-06 23:37:32 +0530

    Update provisioner.sh

--- provisioner.sh
+++ provisioner.sh
@@ -4,18 +4,17 @@
 sudo yum -y update
 
 # install git
-sudo yum install git
+sudo yum -y install git
 
 # install SSM
-sudo yum install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo yum -y install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 #sudo systemctl start amazon-ssm-agent
 
-# install cloudwatch agent
-sudo yum install https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a start
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
+# install CloudWatch agent
+sudo yum -y install https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a start
 
-# install AWS inspector
+# install AWS Inspector
 curl -O https://inspector-agent.amazonaws.com/linux/latest/install
 sudo bash install
-
