24d278719246c01387f0728245deb1321b019f3e
Danish1103 <138514710+Danish1103@users.noreply.github.com>
2023-07-06 18:36:28 +0530

    Update provisioner.sh

--- provisioner.sh
+++ provisioner.sh
@@ -13,6 +13,7 @@ sudo yum install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/
 # install cloudwatch agent
 sudo yum install https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a start
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
 
 # install AWS inspector
 curl -O https://inspector-agent.amazonaws.com/linux/latest/install
